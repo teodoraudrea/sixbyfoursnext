f15f4ae9cfc86080ed7710c1025ecc2a82d601c3
Teodora Udrea <teodora.udrea1998@gmail.com>
2023-03-25 15:57:40 +0000

    added modal button to navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -3,20 +3,29 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import Camera from '@mui/icons-material/Camera';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Camera, ShoppingBag } from '@mui/icons-material';
 
-export default function NavBar() {
+export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+      <AppBar position="sticky">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
             <Camera/>
           </IconButton>
-          <Typography variant="h4" color="inherit" component="div">
+          <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
             6BY4S
           </Typography>
+          <Button color="inherit"><ShoppingBag/></Button>
         </Toolbar>
       </AppBar>
     </Box>
